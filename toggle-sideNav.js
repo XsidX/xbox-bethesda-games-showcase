138951d7a087e44c0972a9830f1d5960bc71b74d
iamsid <developer.sid@outlook.com>
2022-06-22 13:56:39 +0300

    add event listeners to toggle side navigation on menu click

---
+++ toggle-sideNav.js
@@ -0,0 +1,23 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.side-navigation');
+const menuLinks = document.querySelectorAll('.nav-link');
+
+
+function toggleMobileMenu(element) {
+  element.addEventListener('click', () => {
+    const visibility = mobileMenu.getAttribute('data-visible')
+    if (visibility === 'false') {
+      mobileMenu.setAttribute('data-visible', true)
+    } else {
+      mobileMenu.setAttribute('data-visible', false)
+    }
+    hamburgerMenu.classList.toggle('menu-close')
+    document.body.classList.toggle('no-scroll');
+  });
+}
+
+toggleMobileMenu(hamburgerMenu);
+
+menuLinks.forEach((link) => {
+  toggleMobileMenu(link);
+});
